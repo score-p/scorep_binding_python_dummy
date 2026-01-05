030292419adc32438a93bc613d97f29e7b957d1d
Bert Wesarg <bert.wesarg@tu-dresden.de>
2026-01-05 11:52:10 +0100

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='scorep-dummy',
     version='4.4',
-    description='This is a dummy package for the scorep pakcage',
+    description='This is a dummy package for the scorep package',
     author='Andreas Gocht',
     author_email='andreas.gocht@tu-dresden.de',
     url='https://github.com/score-p/scorep_binding_python_dummy',
     long_description='''
 This package only provides the user interface of the Score-P Python tracing.
-The goal is to allow the presence of userinstrumentation in packages even if no Score-P is present.
+The goal is to allow the presence of user instrumentation in packages even if no Score-P is present.
 ''',
     packages=['scorep', 'scorep.instrumenters']
 )
